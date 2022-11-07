b045a1041b4d07aba75e043bc5eba46057ba3e5c
Joao Henrique <jhrodriguesit@gmail.com>
2022-11-07 22:52:05 +1000

    basic game

---
+++ script.js
@@ -0,0 +1,27 @@
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+
+const jump = () => {
+  mario.classList.add("jump");
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+};
+
+const loop = setInterval(() => {
+    console.log('loop')
+    pipePosition = pipe.offsetLeft
+    marioPosition = Number(window.getComputedStyle(mario).bottom.replace('px',' '));
+
+    if(pipePosition <= 100 && pipePosition > 0 && marioPosition <= 60) {
+       pipe.style.animation = 'none'
+       pipe.style.left = `${pipePosition}px`
+
+       mario.style.animation = 'none'
+       mario.style.bottom = `${marioPosition}px`
+
+       clearInterval(loop)
+    }
+}, 10);
+
+document.addEventListener("keydown", jump);
